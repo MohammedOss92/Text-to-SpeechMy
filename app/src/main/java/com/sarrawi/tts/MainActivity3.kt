dbbe1c40b884a80940743c17092bddaee344571a
MohammedOss <abdallahomar1992@gmail.com>
2024-08-21 00:37:25 +0300

    Rooooooooooom

--- app/src/main/java/com/sarrawi/tts/MainActivity3.kt
+++ app/src/main/java/com/sarrawi/tts/MainActivity3.kt
@@ -3,6 +3,10 @@ package com.sarrawi.tts
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.speech.tts.TextToSpeech
+import android.view.View
+import android.widget.ArrayAdapter
+import android.widget.AdapterView
+import android.widget.Spinner
 import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.viewModelScope
 import androidx.recyclerview.widget.LinearLayoutManager
@@ -11,6 +15,7 @@ import com.sarrawi.tts.Adapter.AdapterWord
 import com.sarrawi.tts.VModel.WordVM
 import kotlinx.coroutines.launch
 import java.util.*
+
 
 class MainActivity3 : AppCompatActivity(), TextToSpeech.OnInitListener {
 
@@ -27,7 +32,7 @@ class MainActivity3 : AppCompatActivity(), TextToSpeech.OnInitListener {
         setContentView(R.layout.activity_main2)
 
         recyclerView = findViewById(R.id.recyclerView)
-        setUpRv()
+        setUpRv2()
 // تهيئة TextToSpeech
         tts = TextToSpeech(this, this)
     }
@@ -35,6 +40,18 @@ class MainActivity3 : AppCompatActivity(), TextToSpeech.OnInitListener {
     private fun setUpRv() = wordVM.viewModelScope.launch {
         wordVM.getAllWords().observe(this@MainActivity3) { listShows ->
             adapterWord = AdapterWord(listShows,this@MainActivity3, tts)
+            adapterWord.stateRestorationPolicy = RecyclerView.Adapter.StateRestorationPolicy.ALLOW
+            adapterWord.wordsList = listShows
+            recyclerView.layoutManager = LinearLayoutManager(this@MainActivity3)
+            recyclerView.adapter = adapterWord
+        }
+
+
+    }
+
+    private fun setUpRv2() = wordVM.viewModelScope.launch {
+        wordVM.getAllWords_twoViewModel().observe(this@MainActivity3) { listShows ->
+            adapterWord = AdapterWord(listShows, this@MainActivity3, tts)
             adapterWord.stateRestorationPolicy = RecyclerView.Adapter.StateRestorationPolicy.ALLOW
             adapterWord.wordsList = listShows
             recyclerView.layoutManager = LinearLayoutManager(this@MainActivity3)
