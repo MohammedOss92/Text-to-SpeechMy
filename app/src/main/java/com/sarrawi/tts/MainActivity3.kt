91e68103b2bdb9bb33e0664f33deff53ef0b2519
MohammedOss <abdallahomar1992@gmail.com>
2024-08-21 20:44:33 +0300

    Rooooooooooom

--- app/src/main/java/com/sarrawi/tts/MainActivity3.kt
+++ app/src/main/java/com/sarrawi/tts/MainActivity3.kt
@@ -58,27 +58,31 @@ class MainActivity3 : AppCompatActivity(), TextToSpeech.OnInitListener {
         spinner.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
             override fun onItemSelected(parent: AdapterView<*>, view: View?, position: Int, id: Long) {
                 when (position) {
-                    0 -> setUpRv(wordVM.getAllWords())
-                    1 -> setUpRvTwo(wordVM.getAllWords_twoViewModel())
-                    2 -> setUpRvThree(wordVM.getAllWord_threeViewModel())
-                    3 -> setUpRvTb_Adjectives(wordVM.getAlltb_AdjectivesViewModel())
-                    4 -> setUpRvTb_Clothes_and_toilet_articles(wordVM.getAlltb_Clothes_and_toilet_articlesViewModel())
-                    5 -> setUpRvTb_Colours(wordVM.getAlltb_ColoursViewModel())
-                    6 -> setUpRvTb_Family(wordVM.getAlltb_FamilyViewModel())
-                    7 -> setUpRvTb_fruit(wordVM.getAlltb_fruitViewModel())
-                    8 -> setUpRvTb_Houseandfurniture(wordVM.getAlltb_HouseandfurnitureViewModel())
-                    9 -> setUpRvTb_Human_body(wordVM.getAlltb_Human_bodyViewModel())
-                    10 -> setUpRvTb_Tb_jobs(wordVM.getAlltb_jobsViewModel())
-                    11 -> setUpRvTb_Kitchen_tools(wordVM.getAlltb_Kitchen_toolsViewModel())
-                    12 -> setUpRvTb_Tb_places(wordVM.getAlltb_placesViewModel())
-                    13 -> setUpRvTb_Pronoun(wordVM.getAlltb_PronounViewModel())
-                    14 -> setUpRvTb_school(wordVM.getAlltb_schoolViewModel())
-                    15 -> setUpRvSimilar_words(wordVM.getAlltb_Similar_wordsViewModel())
-                    16 -> setUpRvTb_The_animals(wordVM.getAlltb_The_animalsViewModel())
-                    17 -> setUpRvTb_transports(wordVM.getAlltb_transportsViewModel())
-                    18 -> setUpRvTv_verbs(wordVM.getAlltv_verbsViewModel())
+//                    0 -> setUpRv(wordVM.getAllWords())
+//                    1 -> setUpRvTwo(wordVM.getAllWords_twoViewModel())
+//                    2 -> setUpRvThree(wordVM.getAllWord_threeViewModel())
+//                    3 -> setUpRvTb_Adjectives(wordVM.getAlltb_AdjectivesViewModel())
+//                    4 -> setUpRvTb_Clothes_and_toilet_articles(wordVM.getAlltb_Clothes_and_toilet_articlesViewModel())
+//                    5 -> setUpRvTb_Colours(wordVM.getAlltb_ColoursViewModel())
+//                    6 -> setUpRvTb_Family(wordVM.getAlltb_FamilyViewModel())
+//                    7 -> setUpRvTb_fruit(wordVM.getAlltb_fruitViewModel())
+//                    8 -> setUpRvTb_Houseandfurniture(wordVM.getAlltb_HouseandfurnitureViewModel())
+//                    9 -> setUpRvTb_Human_body(wordVM.getAlltb_Human_bodyViewModel())
+//                    10 -> setUpRvTb_Tb_jobs(wordVM.getAlltb_jobsViewModel())
+//                    11 -> setUpRvTb_Kitchen_tools(wordVM.getAlltb_Kitchen_toolsViewModel())
+//                    12 -> setUpRvTb_Tb_places(wordVM.getAlltb_placesViewModel())
+//                    13 -> setUpRvTb_Pronoun(wordVM.getAlltb_PronounViewModel())
+//                    14 -> setUpRvTb_school(wordVM.getAlltb_schoolViewModel())
+//                    15 -> setUpRvSimilar_words(wordVM.getAlltb_Similar_wordsViewModel())
+//                    16 -> setUpRvTb_The_animals(wordVM.getAlltb_The_animalsViewModel())
+//                    17 -> setUpRvTb_transports(wordVM.getAlltb_transportsViewModel())
+//                    18 -> setUpRvTv_verbs(wordVM.getAlltv_verbsViewModel())
+                    0 -> setUpRecyclerView(wordVM.getAllWords(), SealedClass.Word::class.java)
+                    1 -> setUpRecyclerView(wordVM.getAllWords_twoViewModel(), SealedClass.Words_two::class.java)
+                    2 -> setUpRecyclerView(wordVM.getAllWord_threeViewModel(), SealedClass.Word_three::class.java)
                     // أضف المزيد من الحالات حسب الحاجة
                 }
+
             }
 
             override fun onNothingSelected(parent: AdapterView<*>) {
@@ -106,6 +110,88 @@ class MainActivity3 : AppCompatActivity(), TextToSpeech.OnInitListener {
                     @Suppress("UNCHECKED_CAST")
                     adapterWord.wordsList = listShows as List<SealedClass.Word_three>
                 }
+
+                itemType.isAssignableFrom(SealedClass.Tb_Adjectives::class.java) -> {
+                    @Suppress("UNCHECKED_CAST")
+                    adapterWord.wordsList = listShows as List<SealedClass.Tb_Adjectives>
+                }
+
+                itemType.isAssignableFrom(SealedClass.Tb_Clothes_and_toilet_articles::class.java) -> {
+                    @Suppress("UNCHECKED_CAST")
+                    adapterWord.wordsList = listShows as List<SealedClass.Tb_Clothes_and_toilet_articles>
+                }
+
+                itemType.isAssignableFrom(SealedClass.Tb_Colours::class.java) -> {
+                    @Suppress("UNCHECKED_CAST")
+                    adapterWord.wordsList = listShows as List<SealedClass.Tb_Colours>
+                }
+
+                itemType.isAssignableFrom(SealedClass.Tb_Family::class.java) -> {
+                    @Suppress("UNCHECKED_CAST")
+                    adapterWord.wordsList = listShows as List<SealedClass.Tb_Family>
+                }
+
+                itemType.isAssignableFrom(SealedClass.Tb_fruit::class.java) -> {
+                    @Suppress("UNCHECKED_CAST")
+                    adapterWord.wordsList = listShows as List<SealedClass.Tb_fruit>
+                }
+
+                itemType.isAssignableFrom(SealedClass.Tb_Houseandfurniture::class.java) -> {
+                    @Suppress("UNCHECKED_CAST")
+                    adapterWord.wordsList = listShows as List<SealedClass.Tb_Houseandfurniture>
+                }
+
+                itemType.isAssignableFrom(SealedClass.Tb_Human_body::class.java) -> {
+                    @Suppress("UNCHECKED_CAST")
+                    adapterWord.wordsList = listShows as List<SealedClass.Tb_Human_body>
+                }
+
+                itemType.isAssignableFrom(SealedClass.Tb_Kitchen_tools::class.java) -> {
+                    @Suppress("UNCHECKED_CAST")
+                    adapterWord.wordsList = listShows as List<SealedClass.Tb_Kitchen_tools>
+                }
+
+                itemType.isAssignableFrom(SealedClass.Tb_jobs::class.java) -> {
+                    @Suppress("UNCHECKED_CAST")
+                    adapterWord.wordsList = listShows as List<SealedClass.Tb_jobs>
+                }
+
+                itemType.isAssignableFrom(SealedClass.Tb_places::class.java) -> {
+                    @Suppress("UNCHECKED_CAST")
+                    adapterWord.wordsList = listShows as List<SealedClass.Tb_places>
+                }
+
+                itemType.isAssignableFrom(SealedClass.Tb_Pronoun::class.java) -> {
+                    @Suppress("UNCHECKED_CAST")
+                    adapterWord.wordsList = listShows as List<SealedClass.Tb_Pronoun>
+                }
+
+                itemType.isAssignableFrom(SealedClass.Tb_school::class.java) -> {
+                    @Suppress("UNCHECKED_CAST")
+                    adapterWord.wordsList = listShows as List<SealedClass.Tb_school>
+                }
+
+                itemType.isAssignableFrom(SealedClass.Tb_Similar_words::class.java) -> {
+                    @Suppress("UNCHECKED_CAST")
+                    adapterWord.wordsList = listShows as List<SealedClass.Tb_Similar_words>
+                }
+
+                itemType.isAssignableFrom(SealedClass.Tb_The_animals::class.java) -> {
+                    @Suppress("UNCHECKED_CAST")
+                    adapterWord.wordsList = listShows as List<SealedClass.Tb_The_animals>
+                }
+
+                itemType.isAssignableFrom(SealedClass.Tb_transports::class.java) -> {
+                    @Suppress("UNCHECKED_CAST")
+                    adapterWord.wordsList = listShows as List<SealedClass.Tb_transports>
+                }
+
+                itemType.isAssignableFrom(SealedClass.Tv_verbs::class.java) -> {
+                    @Suppress("UNCHECKED_CAST")
+                    adapterWord.wordsList = listShows as List<SealedClass.Tv_verbs>
+                }
+
+
                 // أضف المزيد من التحقق إذا لزم الأمر
             }
         }
@@ -125,107 +211,6 @@ class MainActivity3 : AppCompatActivity(), TextToSpeech.OnInitListener {
         }
     }
 
-    private fun setUpRvThree(liveData: LiveData<List<SealedClass.Word_three>>) {
-        liveData.observe(this) { listShows ->
-            adapterWord.wordsList = listShows
-        }
-    }
-
-    private fun setUpRvTb_Adjectives(liveData: LiveData<List<SealedClass.Tb_Adjectives>>) {
-        liveData.observe(this) { listShows ->
-            adapterWord.wordsList = listShows
-        }
-    }
-
-    private fun setUpRvTb_Clothes_and_toilet_articles(liveData: LiveData<List<SealedClass.Tb_Clothes_and_toilet_articles>>) {
-        liveData.observe(this) { listShows ->
-            adapterWord.wordsList = listShows
-        }
-    }
-
-    private fun setUpRvTb_Colours(liveData: LiveData<List<SealedClass.Tb_Colours>>) {
-        liveData.observe(this) { listShows ->
-            adapterWord.wordsList = listShows
-        }
-    }
-
-    private fun setUpRvTb_Family(liveData: LiveData<List<SealedClass.Tb_Family>>) {
-        liveData.observe(this) { listShows ->
-            adapterWord.wordsList = listShows
-        }
-    }
-
-    private fun setUpRvTb_fruit(liveData: LiveData<List<SealedClass.Tb_fruit>>) {
-        liveData.observe(this) { listShows ->
-            adapterWord.wordsList = listShows
-        }
-    }
-
-    private fun setUpRvTb_Houseandfurniture(liveData: LiveData<List<SealedClass.Tb_Houseandfurniture>>) {
-        liveData.observe(this) { listShows ->
-            adapterWord.wordsList = listShows
-        }
-    }
-
-    private fun setUpRvTb_Human_body(liveData: LiveData<List<SealedClass.Tb_Human_body>>) {
-        liveData.observe(this) { listShows ->
-            adapterWord.wordsList = listShows
-        }
-    }
-
-    private fun setUpRvTb_Tb_jobs(liveData: LiveData<List<SealedClass.Tb_jobs>>) {
-        liveData.observe(this) { listShows ->
-            adapterWord.wordsList = listShows
-        }
-    }
-
-    private fun setUpRvTb_Kitchen_tools(liveData: LiveData<List<SealedClass.Tb_Kitchen_tools>>) {
-        liveData.observe(this) { listShows ->
-            adapterWord.wordsList = listShows
-        }
-    }
-
-    private fun setUpRvTb_Tb_places(liveData: LiveData<List<SealedClass.Tb_places>>) {
-        liveData.observe(this) { listShows ->
-            adapterWord.wordsList = listShows
-        }
-    }
-
-    private fun setUpRvTb_Pronoun(liveData: LiveData<List<SealedClass.Tb_Pronoun>>) {
-        liveData.observe(this) { listShows ->
-            adapterWord.wordsList = listShows
-        }
-    }
-
-    private fun setUpRvTb_school(liveData: LiveData<List<SealedClass.Tb_school>>) {
-        liveData.observe(this) { listShows ->
-            adapterWord.wordsList = listShows
-        }
-    }
-
-    private fun setUpRvSimilar_words(liveData: LiveData<List<SealedClass.Tb_Similar_words>>) {
-        liveData.observe(this) { listShows ->
-            adapterWord.wordsList = listShows
-        }
-    }
-
-    private fun setUpRvTb_The_animals(liveData: LiveData<List<SealedClass.Tb_The_animals>>) {
-        liveData.observe(this) { listShows ->
-            adapterWord.wordsList = listShows
-        }
-    }
-
-    private fun setUpRvTb_transports(liveData: LiveData<List<SealedClass.Tb_transports>>) {
-        liveData.observe(this) { listShows ->
-            adapterWord.wordsList = listShows
-        }
-    }
-
-    private fun setUpRvTv_verbs(liveData: LiveData<List<SealedClass.Tv_verbs>>) {
-        liveData.observe(this) { listShows ->
-            adapterWord.wordsList = listShows
-        }
-    }
 
     override fun onInit(status: Int) {
         if (status == TextToSpeech.SUCCESS) {
